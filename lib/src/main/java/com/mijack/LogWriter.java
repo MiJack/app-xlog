f6d37728794b8209ca02dcbda892c4b49378ca69
Mi&Jack <mijackstudio@gmail.com>
2018-06-20 16:00:01 +0800

    add code

---
+++ lib/src/main/java/com/mijack/LogWriter.java
@@ -0,0 +1,14 @@
+package com.mijack;
+
+/**
+ * The log file writer
+ *
+ * @author Mi&Jack
+ */
+public class LogWriter {
+
+    public static synchronized void d(int hookId, int pid, int threadId, String msg) {
+        //todo
+    }
+
+}
